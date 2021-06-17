539b0c82cb7a7bf53a4e330d90615189678e0cfd
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-06-17 19:57:41 -0300

    Conteúdo da primeira aula

---
+++ dragonball/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <> 
+      <div className='logoBar'>
+        <img className='logoImg' src={logo} alt='Logo do anime Dragonball Z'></img>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
